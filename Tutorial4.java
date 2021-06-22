4d3a1f2e4ad53fa58e07fdffaab90ac753ca4d00
udaralaksitha <it20188290@my.sliit.lk>
2021-06-22 17:36:09 +0530

    User Inputs

--- Tutorial4.java
+++ Tutorial4.java
@@ -1,4 +1,25 @@
 package Java.practise;
 
+import java.util.Scanner;
+
 public class Tutorial4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("what is your favourite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old ");
+        System.out.println("You like "+food);
+    }
+
 }
+
