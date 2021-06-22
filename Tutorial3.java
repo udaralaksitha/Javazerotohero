8db4ec5874bfbab953989b11e84e6f0863f02b7d
udaralaksitha <it20188290@my.sliit.lk>
2021-06-22 16:30:01 +0530

    swap 2 variables

---
+++ Tutorial3.java
@@ -0,0 +1,19 @@
+package Java.practise;
+
+public class Tutorial3 {
+    public static void main(String[] args) {
+
+        String x = "water";
+        String y = "kool-Aid";
+        String temp;
+
+        temp = x;
+        x=y;
+
+
+        System.out.println("x;"+x);
+        System.out.println("y;"+y);
+
+    }
+}
+
