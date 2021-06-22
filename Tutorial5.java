559e68af7e39cf043ea41cd6a5c2a7e3a606e362
udaralaksitha <it20188290@my.sliit.lk>
2021-06-22 17:39:44 +0530

    Expressions

---
+++ Tutorial5.java
@@ -0,0 +1,28 @@
+package Java.practise;
+
+public class Tutorial5 {
+    public static void main(String[] args) {
+        /*expression = operands & operators
+         *operands= values, variables,numbers,quantity
+         *operators = + - * / %
+         */
+
+        int friends = 8 ;
+        int number = 20;
+
+        friends = friends * 2;
+        number = number %3 ;
+       /* number = number +1;
+          number = number +2;
+
+        */
+        //number +=1;
+        number++;
+        friends -=2;
+
+        System.out.println(friends);
+        System.out.println(number);
+
+    }
+}
+
