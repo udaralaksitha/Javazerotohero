646fccafe42d2d2840946b2c8787e5436cdc2b83
udaralaksitha <it20188290@my.sliit.lk>
2021-06-23 10:36:09 +0530

    Random

---
+++ Tutorial8.java
@@ -0,0 +1,16 @@
+package Java.practise;
+
+import java.util.Random;
+
+public class Tutorial8 {
+    public static void main(String[] args){
+        Random random = new Random();
+
+
+         //int x = random.nextInt();
+         //double y = random.nextDouble();
+        boolean z = random.nextBoolean();
+
+         System.out.println(z);
+    }
+}
