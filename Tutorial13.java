8f7ae104a87ae5db650f97fda95eaee465dd3f11
udaralaksitha <it20188290@my.sliit.lk>
2021-06-23 10:47:40 +0530

    for Loop

---
+++ Tutorial13.java
@@ -0,0 +1,12 @@
+package Java.practise;
+
+public class Tutorial13 {
+    public static void main(String[] args) {
+        //for loop = execute a block of code limited amount of times
+
+        for (int i=10; i >=0;i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
