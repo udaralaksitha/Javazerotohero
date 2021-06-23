f957171ced26ee04506c09754398525a2966745a
udaralaksitha <it20188290@my.sliit.lk>
2021-06-23 10:37:49 +0530

    If Statements

---
+++ Tutorial9.java
@@ -0,0 +1,21 @@
+package Java.practise;
+
+public class Tutorial9 {
+    public static void main(String[] args){
+        //If statement = perform a block of code if it's condition evaluates to be true
+
+        int age = 77;
+        if (age>=75) {
+            System.out.println("Ok Boomer!");
+        }
+        else if(age>=18){
+            System.out.println("You are an adult!");
+        }
+        else if(age>=13){
+            System.out.println("You are a teenager!");
+        }
+        else {
+            System.out.println("You are not an adult!");
+        }
+    }
+}
