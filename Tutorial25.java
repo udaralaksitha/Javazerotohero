b87a6e8c17c49c11434dc26b621b175a480e32a3
udaralaksitha <it20188290@my.sliit.lk>
2021-06-24 23:34:52 +0530

    final keyword

---
+++ Tutorial25.java
@@ -0,0 +1,11 @@
+package Java.practise;
+
+public class Tutorial25 {
+    public static void main(String[] args){
+        final double pi = 3.14789;
+
+        //pi=4;
+
+        System.out.println(pi);
+    }
+}
