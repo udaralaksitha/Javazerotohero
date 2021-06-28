39574b85efccb6afac0ca4e25fbfab08a51bf67c
udaralaksitha <it20188290@my.sliit.lk>
2021-06-29 00:25:02 +0530

    Local & Global

---
+++ Tutorial28.java
@@ -0,0 +1,16 @@
+package Java.practise;
+
+import java.util.Random;
+
+public class Tutorial28 {
+    public static void main(String[] args){
+        //local= declared inside a method
+        //       visible only to that method
+        //global=declared outside a method, but within a class visible to all
+        //       parts of a class
+
+        DiceRoller diceRoller = new DiceRoller();
+
+    }
+}
+
